282963dcc18dfecfb64b22d9208bdb8ef9164e12
Vineet0197 <vineetaggarwal701@gmail.com>
2020-10-15 12:56:05 +0530

    Initial commit

---
+++ SpringSecurityLoginLogoutExample/src/main/java/com/tutorial/configuration/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.tutorial.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+@ComponentScan("com.tutorial")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withDefaultPasswordEncoder()
+				.username("vineet").password("vin1234").roles("ADMIN").build());
+		
+		return manager;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+		.authorizeRequests()
+		.anyRequest().hasRole("ADMIN")
+		.and().formLogin().and()
+		.httpBasic()
+		.and().logout()
+		.logoutUrl("/j_spring_security_logout")
+		.logoutSuccessUrl("/");
+	}
+}
